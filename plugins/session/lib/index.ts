8b336532a61ea01b3b043bd5eeb6a66a1abacba2
max <max>
2023-07-02 21:20:49 +0300

    Fixed exports

--- plugins/session/lib/index.ts
+++ plugins/session/lib/index.ts
@@ -1,18 +1,4 @@
-import { Plugin } from "discord.js-mvc";
-import { Session } from "./Session";
-import { Storage } from "./types";
-export * from './Storage';
-
-export function SessionPlugin(storage: Storage, initialData: Record<string,string>): Plugin{
-    return (ctx) => {
-        if (ctx.isInteraction()){
-            
-            const userID = ctx.interaction.user.id;
-            (ctx as any).session = new Session(storage, initialData, userID);
-        }
-        else if (ctx.isMessage()){
-            const userID = ctx.message.author.id;
-            (ctx as any).session = new Session(storage, initialData, userID);
-        }
-    }
-}
+export { SessionPlugin } from './Plugin'
+export { Session } from './Session'
+export { SessionFlavor, Storage } from './types'
+export { FileStorage, InMemoryStorage } from './Storage'
