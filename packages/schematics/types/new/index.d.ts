cfeb84d609d809d5dd195ad7d4b6e9047005caaf
max <max.vojtkov@gmail.com>
2023-07-07 10:55:11 +0300

    cleaned

--- packages/schematics/types/new/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-import { NewOptions } from './options';
-export declare function newBot(_options: NewOptions): Rule;
-//# sourceMappingURL=index.d.ts.map
